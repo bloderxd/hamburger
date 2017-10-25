9de4b4646d8d73b4d604971eda6fd996be05609d
Bloder <aptgetinstalldanielleme@gmail.com>
2017-10-25 21:29:58 -0200

    Initial commit

---
+++ app/src/main/java/com/example/bloder/hamburger/HamburgersActivity.java
@@ -0,0 +1,13 @@
+package com.example.bloder.hamburger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HamburgersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hamburgers);
+    }
+}
