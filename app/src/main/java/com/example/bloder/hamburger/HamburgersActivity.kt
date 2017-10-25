6a5bc64df0848f21798379fd4310a7af6d45968d
Bloder <aptgetinstalldanielleme@gmail.com>
2017-10-25 21:54:39 -0200

    Create kotlin main activity

---
+++ app/src/main/java/com/example/bloder/hamburger/HamburgersActivity.kt
@@ -0,0 +1,15 @@
+package com.example.bloder.hamburger
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by bloder on 25/10/17.
+ */
+class HamburgersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hamburgers)
+    }
+}
