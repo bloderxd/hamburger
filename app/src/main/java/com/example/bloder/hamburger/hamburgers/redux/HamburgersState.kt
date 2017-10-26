311fb38d8a7f69123268e7061d71c9d65d3765ac
Bloder <aptgetinstalldanielleme@gmail.com>
2017-10-25 22:09:45 -0200

    Create hamburgers state

---
+++ app/src/main/java/com/example/bloder/hamburger/hamburgers/redux/HamburgersState.kt
@@ -0,0 +1,8 @@
+package com.example.bloder.hamburger.hamburgers.redux
+
+import com.example.bloder.hamburger.redux.State
+
+/**
+ * Created by bloder on 25/10/17.
+ */
+data class HamburgersState(val value: Any) : State
