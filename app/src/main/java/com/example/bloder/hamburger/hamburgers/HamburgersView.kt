8a1a691f09ae3ca4ea6d2201bc45fe21af488bcb
Bloder <aptgetinstalldanielleme@gmail.com>
2017-10-25 22:11:57 -0200

    Create hamburgers view

---
+++ app/src/main/java/com/example/bloder/hamburger/hamburgers/HamburgersView.kt
@@ -0,0 +1,19 @@
+package com.example.bloder.hamburger.hamburgers
+
+import android.content.Context
+import com.example.bloder.hamburger.hamburgers.redux.HamburgersState
+import com.example.bloder.hamburger.redux.ReactView
+import com.reduks.reduks.Store
+import com.reduks.reduks.reduksStore
+
+/**
+ * Created by bloder on 25/10/17.
+ */
+class HamburgersView(context: Context) : ReactView<HamburgersState>(context) {
+
+    override fun render(state: HamburgersState) {
+
+    }
+
+    override fun buildStore(): Store<HamburgersState> = reduksStore {}
+}
