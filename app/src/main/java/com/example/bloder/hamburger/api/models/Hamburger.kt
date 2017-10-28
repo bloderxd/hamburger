c1ed668eef639c65e8496ec69d89ebc10864a411
Bloder <aptgetinstalldanielleme@gmail.com>
2017-10-28 15:36:02 -0200

    Make hamburger fields public

--- app/src/main/java/com/example/bloder/hamburger/api/models/Hamburger.kt
+++ app/src/main/java/com/example/bloder/hamburger/api/models/Hamburger.kt
@@ -4,8 +4,8 @@ package com.example.bloder.hamburger.api.models
  * Created by bloder on 28/10/17.
  */
 data class Hamburger(
-        private val id: Int,
-        private val name: String,
-        private val ingredients: List<Int>,
-        private val image: String
+        val id: Int,
+        val name: String,
+        val ingredients: List<Int>,
+        val image: String
 )
