3777b2ec2e49cb2a33949102942a7ccdadb9d110
Bloder <aptgetinstalldanielleme@gmail.com>
2017-10-28 15:36:21 -0200

    Make offer fields public

--- app/src/main/java/com/example/bloder/hamburger/api/models/Offer.kt
+++ app/src/main/java/com/example/bloder/hamburger/api/models/Offer.kt
@@ -4,6 +4,6 @@ package com.example.bloder.hamburger.api.models
  * Created by bloder on 28/10/17.
  */
 data class Offer(
-        private val id: Int,
-        private val description: String
+        val id: Int,
+        val description: String
 )
