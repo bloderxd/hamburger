3bdc7b24376f47275a99fef99da4fdb6ad10bf66
Bloder <aptgetinstalldanielleme@gmail.com>
2017-10-28 15:36:42 -0200

    Create payload to pojo's tests

--- app/src/test/java/com/example/bloder/hamburger/ApiMappingTest.kt
+++ app/src/test/java/com/example/bloder/hamburger/ApiMappingTest.kt
@@ -1,6 +1,9 @@
 package com.example.bloder.hamburger
 
 import com.example.bloder.hamburger.api.payloads.CartPayload
+import com.example.bloder.hamburger.api.payloads.HamburgerPayload
+import com.example.bloder.hamburger.api.payloads.IngredientPayload
+import com.example.bloder.hamburger.api.payloads.OfferPayload
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -17,14 +20,39 @@ class ApiMappingTest : Spek({
 
     given("Payloads to model") {
 
-        it("Should map correct cart object") {
-            val cartModel = CartPayload(2, 1, listOf(1, 2), 1509148800).toModel()
+        it("Should map correct Cart object") {
+            val model = CartPayload(2, 1, listOf(1, 2), 1509148800).toModel()
             assertTrue {
-                cartModel.id == 2
-                        && cartModel.hamburgerId == 1
-                        && cartModel.extras.isNotEmpty()
-                        && cartModel.date.trim().toLowerCase() == "27/10/2017"
+                model.id == 2
+                        && model.hamburgerId == 1
+                        && model.extras.isNotEmpty()
+                        && model.date.trim().toLowerCase() == "27/10/2017"
             }
+        }
+
+        it("Should map correct Hamburger object") {
+            val model = HamburgerPayload(0, "bacon", listOf(), "image").toModel()
+            assertTrue {
+                model.id == 0
+                        && model.name.trim().toLowerCase() == "bacon"
+                        && model.ingredients.isEmpty()
+                        && model.image.trim().toLowerCase() == "image"
+            }
+        }
+
+        it("Should map correct Ingredient object") {
+            val model = IngredientPayload(1, "BBQ", 1.5, "image").toModel()
+            assertTrue {
+                model.id == 1
+                        && model.name.trim().toLowerCase() == "bbq"
+                        && model.price == 1.5
+                        && model.image.trim().toLowerCase() == "image"
+            }
+        }
+
+        it("Should map correct Offer object") {
+            val model = OfferPayload(2, "this is a description").toModel()
+            assertTrue { model.id == 2 && model.description.trim().toLowerCase() == "this is a description" }
         }
 
     }
