ccadd72a55bb505a94551e7d9c1e8e153b855ea8
Bloder <aptgetinstalldanielleme@gmail.com>
2017-11-01 22:26:55 -0200

    Create offer total calculator

---
+++ app/src/main/java/com/example/bloder/hamburger/hamburger_details/offer_controller/OFFER.kt
@@ -0,0 +1,8 @@
+package com.example.bloder.hamburger.hamburger_details.offer_controller
+
+/**
+ * Created by bloder on 01/11/17.
+ */
+enum class OFFER {
+    LIGHT, MUCH_MEAT, MUCH_CHEESE, NO_OFFER
+}
